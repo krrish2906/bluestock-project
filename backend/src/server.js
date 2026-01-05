f2051be40ed044066af659f5ed530be439c1a129
krrish2906 <krrishbansal290605@gmail.com>
2026-01-05 22:06:56 +0530

    CORS update

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,15 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(helmet());
-app.use(cors());
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",
+            "https://bluestock-project-frontend.onrender.com"
+        ],
+        credentials: true,
+    })
+);
 app.use(compression());
 app.use(express.json());
 
