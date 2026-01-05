9591e8912e5dc46ffd4fd81e4ba459bc779e2ef7
krrish2906 <krrishbansal290605@gmail.com>
2026-01-05 18:23:05 +0530

    pg database config changes

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -4,12 +4,17 @@ import dotenv from "dotenv";
 dotenv.config();
 const { Pool } = pkg;
 
+// const pool = new Pool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     port: 5432,
+// });
+
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: 5432,
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
 });
 
 export default pool;
