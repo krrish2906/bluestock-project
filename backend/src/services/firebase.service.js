b27667dc6d425fcbbdc5821177e1894c4416c2aa
krrish2906 <krrishbansal290605@gmail.com>
2026-01-05 18:52:56 +0530

    changes

--- backend/src/services/firebase.service.js
+++ backend/src/services/firebase.service.js
@@ -1,26 +1,42 @@
+// import admin from "firebase-admin";
+// import fs from "fs";
+// import path from "path";
+// import { fileURLToPath } from "url";
+
+// /* ES module equivalent of __dirname */
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// /* Read Firebase service account JSON safely */
+// const serviceAccountPath = path.join(
+//     __dirname,
+//     "../config/firebase-admin.json"
+// );
+
+// const serviceAccount = JSON.parse(
+//     fs.readFileSync(serviceAccountPath, "utf8")
+// );
+
+// if (!admin.apps.length) {
+//     admin.initializeApp({
+//         credential: admin.credential.cert(serviceAccount),
+//     });
+// }
+
+// export const firebaseAuth = admin.auth();
+
+
 import admin from "firebase-admin";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
-
-/* ES module equivalent of __dirname */
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-/* Read Firebase service account JSON safely */
-const serviceAccountPath = path.join(
-    __dirname,
-    "../config/firebase-admin.json"
-);
-
-const serviceAccount = JSON.parse(
-    fs.readFileSync(serviceAccountPath, "utf8")
-);
 
 if (!admin.apps.length) {
     admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
+        credential: admin.credential.cert({
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+        }),
     });
 }
 
 export const firebaseAuth = admin.auth();
+export default admin;
